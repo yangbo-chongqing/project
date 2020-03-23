db3960b3519a1141d0e8f2ea19861c2076bdb83e
yangbo <812507557@qq.com>
2020-03-23 21:16:10 +0800

    v1.05 addsmallpic

--- vue_app_00_null/src/components/exam/pic.vue
+++ vue_app_00_null/src/components/exam/pic.vue
@@ -1,32 +1,84 @@
 <template>
-  <div>
-    <div class="fpic"></div>
-    <img
-      src="http://d.musicapp.migu.cn/ugcdata/playListimg/7c5113ba-f115-4ab4-aadc-f29363492f13.jpg"
-      alt
-    />
+  <div class="fpic">
+    <a href>
+      <div class="imgp" @mousemove="play" @mouseout="unplay">
+        <img
+          class="firstimg"
+          src="http://d.musicapp.migu.cn/ugcdata/playListimg/7c5113ba-f115-4ab4-aadc-f29363492f13.jpg"
+          alt
+        />
+        <p>
+          <img src="../../img/耳机.png" alt />
+          {{read}}
+        </p>
+        <img
+          id="p3"
+          class="lastimg"
+          src="//cdnmusic.migu.cn/v3/static/img/common/default/btn-play.png"
+          alt
+        />
+      </div>
+    </a>
   </div>
 </template>
+<script>
+export default {
+  props: ["pig", "read"],
+  methods: {
+    play() {
+      let p3 = document.getElementById("p3");
+      p3.style.display = "block";
+    },
+    unplay() {
+      let p3 = document.getElementById("p3");
+      p3.style.display = "none";
+    }
+  }
+};
+</script>
 <style  scoped>
 .fpic {
-  width: 150px;
-  background-image: url(http://d.musicapp.migu.cn/ugcdata/playListimg/7c5113ba-f115-4ab4-aadc-f29363492f13.jpg);
-  height: 150px;
-  background-size: 100%;
+  overflow: hidden;
+  width: 160px;
 }
-.fpic:hover {
-  background-size: 120%;
-}
-img {
-  width: 12%;
+.firstimg {
+  width: 100%;
   opacity: 1;
   padding: 0;
   margin: 0;
   transform: scale(1);
   transition: transform 1s ease 0s;
 }
-img:hover {
+.firstimg:hover {
   opacity: 0.9;
   transform: scale(1.05);
+}
+.fpic p img {
+  width: 0.7rem;
+}
+.imgp {
+  position: relative;
+  width: 160px;
+  overflow: hidden;
+}
+.fpic p {
+  font-size: 12px;
+  width: 40%;
+  position: absolute;
+  top: 0.7rem;
+  margin: 0;
+  right: 1rem;
+  color: rgba(240, 248, 255, 0.87);
+  border-radius: 12px;
+  background-color: rgba(0, 0, 0, 0.4);
+  padding: 0 8px;
+  line-height: 24px;
+}
+.lastimg {
+  position: absolute;
+  top: 35%;
+  left: 35%;
+  width: 30%;
+  display: none;
 }
 </style>
--- vue_app_00_null/src/components/home.vue
+++ vue_app_00_null/src/components/home.vue
@@ -2,7 +2,7 @@
   <div>
     <tit></tit>
     <lunbo></lunbo>
-    <pic></pic>
+    <pic v-for="(item,index) in pig" :key="index" :pig="item" :read="read[index]"></pic>
   </div>
 </template>
 <script>
@@ -10,6 +10,15 @@ import tit from "./exam/tit.vue";
 import lunbo from "./exam/lunbo.vue";
 import pic from "./exam/pic.vue";
 export default {
+  data() {
+    return {
+      pig: [
+        "http://d.musicapp.migu.cn/ugcdata/playListimg/7c5113ba-f115-4ab4-aadc-f29363492f13.jpg",
+        "http://d.musicapp.migu.cn/ugcdata/playListimg/9e254b35-18fe-470e-b3b7-0985536d96ee.jpg"
+      ],
+      read: ["173.5W", "121W"]
+    };
+  },
   components: {
     tit,
     lunbo,
